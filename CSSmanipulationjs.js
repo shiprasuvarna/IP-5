a0f5b06795fcc368f340f3c050ed794aa795c7d2
Shipra Suvarna <shipraasuvarna@gmail.com>
2023-11-02 23:13:02 +0530

    CSS manipulation javascript code

---
+++ CSSmanipulationjs.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.getElementById('my-button');
+  
+    button.addEventListener('click', function () {
+      // Add the 'red' class to the button when clicked
+      button.classList.add('red');
+    });
+  });
+  
