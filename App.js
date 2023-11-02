530deb5b23d77b86fc73d7024d1f23d7881d7028
Shipra Suvarna <shipraasuvarna@gmail.com>
2023-11-03 00:23:32 +0530

    props and state
    replace this code with the app.js of survey form / survey question

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'; // You can remove this line if not needed.
+import SurveyForm from './SurveyForm';
+
+function App() {
+  return (
+    <div className="App">
+      <SurveyForm />
+    </div>
+  );
+}
+
+export default App;
+
+
+//surveyform ka app.js
